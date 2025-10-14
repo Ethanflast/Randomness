da5af75ca6a745233f5cf7c00fffceed4ec6ed0b
Ethan <ethanflast@gmail.com>
2025-10-14 16:56:45 +0700

    Game window and image render

---
+++ Randomness/main.cpp
@@ -0,0 +1,42 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+
+#include "Game.h"
+
+int main(int argc, char* args[])
+{
+	if (SDL_Init(SDL_INIT_VIDEO) > 0)
+		std::cout << "HEY.. SDL_Init HAS FAILED. SDL_ERROR: " << SDL_GetError() << std::endl;
+
+	if (!(IMG_Init(IMG_INIT_PNG)))
+		std::cout << "IMG_init has failed. Error: " << SDL_GetError() << std::endl;
+
+	RenderWindow window("GAME v1.0", 1280, 720);
+
+	SDL_Texture* grassTexture = window.loadTexture("images/dark-blur-blurred-gradient.png");
+
+	bool gameRunning = true;
+
+	SDL_Event event;
+
+	while (gameRunning)
+	{
+		// Get our controls and events
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+				gameRunning = false;
+		}
+
+		window.clear();
+		window.render(grassTexture);
+		window.display();
+
+	}
+
+	window.cleanUp();
+	SDL_Quit();
+
+	return 0;
+}
